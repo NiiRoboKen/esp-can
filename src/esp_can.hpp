15914d4c8bf22c1fc330ff4b529fb29b0ad72e7a
Sootta <soutaaibara@gmail.com>
2025-11-16 02:00:33 +0900

    feat/twaiTimingConfig()

--- src/esp_can.hpp
+++ src/esp_can.hpp
@@ -6,17 +6,18 @@
 
 class CanDriver {
     public:
-    bool begin(gpio_num_t tx = GPIO_NUM_27, gpio_num_t rx = GPIO_NUM_26);
+    bool begin(long baudRate = 1E6, gpio_num_t tx = GPIO_NUM_27, gpio_num_t rx = GPIO_NUM_26);
     bool send(uint32_t id, uint8_t data[8], uint8_t dlc);
     void onReceive(void (*callback)(twai_message_t msg));
 
     private:
+    twai_timing_config_t twaiTimingConfig(long baudRate);
     static void rxTask(void* param);
     void (*rxCallback)(twai_message_t msg) = nullptr;
 };
 
 
-bool CanDriver::begin(gpio_num_t tx, gpio_num_t rx) {
+bool CanDriver::begin(long baudRate, gpio_num_t tx, gpio_num_t rx) {
     twai_general_config_t g_config = {
         .mode = TWAI_MODE_NORMAL,   // 通常モード
         .tx_io = tx,
@@ -30,18 +31,16 @@ bool CanDriver::begin(gpio_num_t tx, gpio_num_t rx) {
         .intr_flags = 0
     };
 
-    // 速度設定（500kbps）
-    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    // 速度設定
+    twai_timing_config_t t_config = twaiTimingConfig(baudRate);
     // フィルタ設定（全受信）
     twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
     if (twai_driver_install(&g_config, &t_config, &f_config) != ESP_OK) {
-
         return false;
     }
 
     if (twai_start() != ESP_OK) {
-    
         return false;
     }
 
@@ -69,6 +68,30 @@ bool CanDriver::send(uint32_t id, uint8_t data[8], uint8_t dlc) {
       return true;
     } else {
       return false;
+    }
+}
+
+twai_timing_config_t CanDriver::twaiTimingConfig(long baudRate) {
+    switch (baudRate)
+    {
+    case (long)25E3:
+        return TWAI_TIMING_CONFIG_25KBITS();
+    case (long)50E3:
+        return TWAI_TIMING_CONFIG_50KBITS();
+    case (long)100E3:
+        return TWAI_TIMING_CONFIG_100KBITS();
+    case (long)125E3:
+        return TWAI_TIMING_CONFIG_125KBITS();
+    case (long)250E3:
+        return TWAI_TIMING_CONFIG_250KBITS();
+    case (long)500E3:
+        return TWAI_TIMING_CONFIG_500KBITS();
+    case (long)800E3:
+        return TWAI_TIMING_CONFIG_800KBITS();
+    case (long)1000E3:
+        return TWAI_TIMING_CONFIG_1MBITS();
+    default:
+        return TWAI_TIMING_CONFIG_1MBITS();
     }
 }
 
